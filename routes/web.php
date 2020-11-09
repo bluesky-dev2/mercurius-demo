1e251fa24bce1865dda03511531e7445d44c4bbd
bluesky-dev2 <newtheme@outlook.com>
2020-11-09 21:46:43 +0800

    Added the user slug

--- routes/web.php
+++ routes/web.php
@@ -27,41 +27,41 @@ Auth::routes();
 
 Route::get('/usermessages', 'HomeController@userMessages');
 
-
-Route::group([
-    'as'         => 'mercurius.',
-    'namespace'  => '\App\Http\Controllers\Mercurius',
-    'middleware' => [
-        // 'Mercurius',
-        'web',
-        'auth',
-    ],
-], function () {
-
-    // Mercurius home
-    Route::get('/{userSlug}/messages', function ($userSlug = null) {
-        return View('mercurius::mercurius', [ 'userSlug' => $userSlug]);
-    })->name('home');
-
-    // User Profile
-    Route::get('/{userSlug}/profile/refresh', 'ProfileController@refresh');
-    Route::get('/{userSlug}/profile/notifications', 'ProfileController@notifications');
-    Route::post('/{userSlug}/profile', 'ProfileController@update');
-
-    // Conversations
-    Route::get('/{userSlug}/conversations', 'ConversationsController@index');
-    Route::post('/{userSlug}/conversations/{receiver}', 'ConversationsController@show');
-    Route::delete('/{userSlug}/conversations/{receiver}', 'ConversationsController@destroy');
-
-    // Messages
-    Route::post('/{userSlug}/messages', 'MessagesController@send');
-    Route::delete('/{userSlug}/messages/{id}', 'MessagesController@destroy');
-
-    // Find Receivers
-    Route::post('/{userSlug}/receivers', 'ReceiversController@search');
-
-    // Dummy page example
-    Route::get('/notification-page-sample', function () {
-        return View('mercurius::example');
-    })->name('example');
-});
+//
+//Route::group([
+//    'as'         => 'mercurius.',
+//    'namespace'  => '\App\Http\Controllers\Mercurius',
+//    'middleware' => [
+//        // 'Mercurius',
+//        'web',
+//        'auth',
+//    ],
+//], function () {
+//
+//    // Mercurius home
+//    Route::get('/{userSlug}/messages', function ($userSlug = null) {
+//        return View('mercurius::mercurius', [ 'userSlug' => $userSlug]);
+//    })->name('home');
+//
+//    // User Profile
+//    Route::get('/{userSlug}/profile/refresh', 'ProfileController@refresh');
+//    Route::get('/{userSlug}/profile/notifications', 'ProfileController@notifications');
+//    Route::post('/{userSlug}/profile', 'ProfileController@update');
+//
+//    // Conversations
+//    Route::get('/{userSlug}/conversations', 'ConversationsController@index');
+//    Route::post('/{userSlug}/conversations/{receiver}', 'ConversationsController@show');
+//    Route::delete('/{userSlug}/conversations/{receiver}', 'ConversationsController@destroy');
+//
+//    // Messages
+//    Route::post('/{userSlug}/messages', 'MessagesController@send');
+//    Route::delete('/{userSlug}/messages/{id}', 'MessagesController@destroy');
+//
+//    // Find Receivers
+//    Route::post('/{userSlug}/receivers', 'ReceiversController@search');
+//
+//    // Dummy page example
+//    Route::get('/notification-page-sample', function () {
+//        return View('mercurius::example');
+//    })->name('example');
+//});
